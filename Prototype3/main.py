bc3c2630043b8d22fcdecf315a8c3839ebefc45b
Evan <84354488+kerryev@users.noreply.github.com>
2022-03-18 23:32:23 -0400

    Create main.py

---
+++ Prototype3/main.py
@@ -0,0 +1,38 @@
+# Evan Kerr
+# kerre10
+# Thurs-42
+# -----------------------------------------------------------
+
+# REFINED CODE 3
+
+from time import sleep
+from alert import start, cursor_selection
+import serial
+
+ser = serial.Serial('/dev/2022')
+ser.flush_Input()
+
+# Pyautogiu is a downloadable library that has access
+# to cursor controls and commmands (ie. click, scroll, etc.)
+
+# Serial is a common API ised to read serial outputs from a raspberry pi.
+
+# ------------------------------------------------------------
+
+
+start()
+
+while True:
+    try:
+        # Reads line from serial output
+        ser_bytes = ser.readline()
+
+        # Puts this value into a reaedable format
+        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+
+        cursor_selection(decoded_bytes)
+
+        sleep(1)
+
+    except RuntimeError as error:
+        continue
