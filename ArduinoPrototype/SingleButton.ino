0fa4b9245c7c1be0deaa026dc4f73a116b1464ad
Evan <84354488+kerryev@users.noreply.github.com>
2022-03-20 11:49:50 -0400

    Update SingleButton.ino

--- ArduinoPrototype/SingleButton.ino
+++ ArduinoPrototype/SingleButton.ino
@@ -1,17 +1,22 @@
 const int pinButton1 = 8;
+const int pinButton2 = 7;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  
   int stateButton1 = digitalRead(pinButton1);
+  int stateButton2 = digitalRead(pinButton2);
   
   if (stateButton1 == 1) {
     Serial.println("BUTTON 1 WAS PRESSED");
+  } else if (stateButton2 == 1) {
+    Serial.println("BUTTON 2 WAS PRESSED");
   } else {
     Serial.println("BUTTON WAS NOT PRESSED");
   }
-  delay(1000);
+  delay(500);
 
 }
